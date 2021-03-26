621aa3f98777cccbb7f373bd05fa72de4ffcb8c9
Michelle Berta <bertafamily@sbcglobal.net>
2021-03-26 16:58:46 -0500

    reworked files

---
+++ script.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+  $(".modal").modal();
+});
+
+$(document).ready(function () {
+  $(".carousel").carousel();
+});
+
+$(document).ready(function () {
+  $(".collapsible").collapsible();
+});
+
+/*
+// Access toggle switch HTML element
+var themeSwitcher = document.querySelector("#theme-switcher");
+var container = document.querySelector(".container");
+
+// Set default mode to dark
+var mode = "dark";
+
+// Listen for a click event on toggle switch
+themeSwitcher.addEventListener("click", function () {
+  // If mode is dark, apply light background
+  if (mode === "dark") {
+    mode = "light";
+    container.setAttribute("class", "light");
+  }
+  // If mode is light, apply dark background
+  else {
+    mode = "dark";
+    container.setAttribute("class", "dark");
+  }
+});
+*/
