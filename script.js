eafedc1becd10287bf2d580233b3d810b864f703
Michelle Berta <bertafamily@sbcglobal.net>
2021-04-01 15:53:29 -0500

    began work on light switch

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 $(document).ready(function () {
   $(".carousel").carousel();
-});
-
-function () {
   $(".collapsible").collapsible();
-};
+
+  // Access toggle switch HTML element
+  var themeSwitcher = document.querySelector("#theme-switcher");
+  var container = document.querySelector(".container");
+
+  // Set default mode to dark...this is changed
+  var darkMode = true;
+
+  // Listen for a click event on toggle switch
+  themeSwitcher.addEventListener("click", function () {
+    darkMode = !darkMode;
+    // If mode is dark, apply light background
+    if (!darkMode) {
+      container.setAttribute("class", "bright");
+    } else {
+      //remove bright class//
+      container.removeAttribute("class", "bright");
+    }
+  });
+});
 
 /*
-// Access toggle switch HTML element
-var themeSwitcher = document.querySelector("#theme-switcher");
-var container = document.querySelector(".container");
-
-// Set default mode to dark
-var mode = "dark";
-
-// Listen for a click event on toggle switch
-themeSwitcher.addEventListener("click", function () {
-  // If mode is dark, apply light background
-  if (mode === "dark") {
-    mode = "light";
-    container.setAttribute("class", "light");
-  }
-  // If mode is light, apply dark background
-  else {
-    mode = "dark";
-    container.setAttribute("class", "dark");
-  }
-});
-*/
+}); //this needs to be put back on line 4 */
