c5e104d9c06feef27ac52588355a64fedab5338f
Michelle Berta <bertafamily@sbcglobal.net>
2021-04-02 11:37:23 -0500

    added drop down menu, worked on slider, mobile nav and carousel

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 $(document).ready(function () {
   $(".carousel").carousel();
+  $('.materialboxed').materialbox();
   $(".collapsible").collapsible();
+  $(".dropdown-trigger").dropdown();
+  $('.sidenav').sidenav();
 
   // Access toggle switch HTML element
   var themeSwitcher = document.querySelector("#theme-switcher");
   var container = document.querySelector(".container");
 
-  // Set default mode to dark...this is changed
+  // Set default mode to dark
   var darkMode = true;
 
   // Listen for a click event on toggle switch
   themeSwitcher.addEventListener("click", function () {
-    console.log(container);
     darkMode = !darkMode;
-    // If mode is dark, apply light background
+    // If mode is dark, remove dark, apply bright background
     if (!darkMode) {
       container.classList.remove("dark");
       container.classList.add("bright");
     } else {
-      //remove bright class//
+      //remove bright, apply dark//
       container.classList.remove("bright");
       container.classList.add("dark");
     }
   });
 });
 
-/*
-}); //this needs to be put back on line 4 */
+
