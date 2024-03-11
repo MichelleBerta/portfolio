801e00dc47bb41d6c459c1d558438da1532c338d
Michelle Berta <bertafamily@sbcglobal.net>
2024-03-11 18:16:34 -0500

    revert Eldred changes

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-$(document).ready(function(){
-  $('.sidenav').sidenav();
-});
-
- $(document).ready(function(){
-    $('.collapsible').collapsible();
+$(document).ready(function () {
+  $(".carousel").carousel();
+  $(".materialboxed").materialbox();
+  $(".collapsible").collapsible();
+  $(".dropdown-trigger").dropdown();
+  $(".sidenav").sidenav();
+  $('.modal').modal();
+    
   });
-      
 
-  $(document).ready(function(){
-    $('.carousel').carousel();
+
+  // Access toggle switch HTML element
+  var themeSwitcher = document.querySelector("#theme-switcher");
+  var container = document.querySelector(".container");
+
+  // Set default mode to dark
+  var darkMode = true;
+
+  // Listen for a click event on toggle switch
+  themeSwitcher.addEventListener("click", function () {
+    darkMode = !darkMode;
+    // If mode is dark, remove dark, apply bright background
+    if (!darkMode) {
+      container.classList.remove("dark");
+      container.classList.add("bright");
+    } else {
+      //remove bright, apply dark//
+      container.classList.remove("bright");
+      container.classList.add("dark");
+    }
   });
+;
